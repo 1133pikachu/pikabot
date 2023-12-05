4d382d926144557393aa9c766d1e0af0b24af96b
ぴか <114275947+1133pikachu@users.noreply.github.com>
2023-12-05 20:38:26 +0900

    Add files via upload

---
+++ ping.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('現在のpingを計測することができます。'),
+	async execute(interaction) {
+		const sent = await interaction.reply({ content: '計測中...', fetchReply: true });
+		interaction.editReply(`現在のping:ping_pong:: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+	},
+};
