d162fb60acf90a02046667996ab025984f215e79
ぴか <114275947+1133pikachu@users.noreply.github.com>
2024-07-22 11:58:15 +0900

    v1.1.0

---
+++ sub-systems/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  genshinReplay: require('./genshin-replay'),
+  omikuji: require('./omikuji'),
+  ping: require('./ping'),
+  server: require('./server'),
+  user: require('./user'),
+  translate: require('./translation')
+}
